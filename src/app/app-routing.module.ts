16a53a4d461405888a935142e7c2140144cd9e3a
João Vitor Perin <joao.perin@inteliger.com.br>
2024-10-02 20:46:50 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+    import { AppLayoutComponent } from "./layout/app.layout.component";
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            {
+                path: '', component: AppLayoutComponent,
+                children: []
+            }
+        ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
