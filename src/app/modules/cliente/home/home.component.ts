f01196959445169ffd9b95229a1000a1b822f5c6
Joao Vitor Perin <joao.perin@inteliger.com.br>
2025-03-16 13:32:51 -0300

    teste

--- src/app/modules/cliente/home/home.component.ts
+++ src/app/modules/cliente/home/home.component.ts
@@ -23,6 +23,7 @@ export class HomeComponent implements OnInit{
     private clienteService: ClienteService,
     private modalService: ModalService,
   ){
+    //teste
     this.columns = [
       {
         dataField: 'telefone',
